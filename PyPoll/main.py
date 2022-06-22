90b5f16ae6cf56584ecc387b4a35c5b15a633175
shola Daropale <sholadennis@yahoo.com>
2022-06-22 03:20:31 +0200

    completed the number of voters calculation

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,11 +7,13 @@ import csv
 total_vote = []
 voters = 0
 candidates_list = []
-candidates = 0
+
 percentage_vote_candidate = 0
 each_candidate_vote = 0
 winner = 0
-candidate1 = int(Diana DeGette)
+
+candidate_vote = []
+candidates = 0
 
 
 # Path to collect data from the Resources folder
@@ -25,18 +27,25 @@ with open(csvpath) as csvfile:
 
     header = next(csvreader)
 
+
     #loop through the data after the deader row
     for rows in csvreader:
         total_vote.append(int(rows[0]))
 
         candidates_list.append(str(rows[2]))     
 
-        
-
+    
     voters = len(total_vote)
-    candidates = len(candidates_list)
+    candidates = (candidates_list)
   
-        
+    
+    #
+    
+#candidate_name = rows[2]  
 
-    print(f'Total Votes:  {str(voters)}')
-    #print(f' {candidates}')
+#name_count = rows[2].count("Charles Casper Stockham")
+
+#counted = len(name_count)
+
+print(f'Total Votes:  {str(voters)}')
+#print(f' {counted}')
