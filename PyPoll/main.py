5f28b2ac63723d81857c5e142f0481cf7e5b13df
shola Daropale <sholadennis@yahoo.com>
2022-06-23 04:29:10 +0200

    calculate the percentage and count for candidates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,12 +8,9 @@ vote = []
 total_votes = 0
 
 
-percentage_vote_candidate = {}
-each_candidate_vote = 0
-winner = 0
-candidate_name = []
-candidate_vote = {}
-candidates = []
+Charles_count = 0
+Diana_count = 0
+Raymon_count = 0
 
 
 # Path to collect data from the Resources folder
@@ -25,6 +22,7 @@ with open(csvpath) as csvfile:
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
 
+    #Skip header row
     header = next(csvreader)
 
 
@@ -35,7 +33,26 @@ with open(csvpath) as csvfile:
         #total votes casted
         total_votes = len(vote)
 
-            
+        #Count votes for diana
+        if rows[2] == "Diana DeGette":
+            Diana_count += 1
+        #Count votes for charles
+        if rows[2] == "Charles Casper Stockham":
+            Charles_count += 1
+        #Count votes for raymon
+        if rows[2] == "Raymon Anthony Doane":
+            Raymon_count += 1
+        
+        #calculate the percentage of votes won by each candidate 
+        Diana_votes_percentage = round(((Diana_count / total_votes) * 100), 3)
+        Charles_votes_percentage = round(((Charles_count / total_votes) * 100), 3)
+        Raymon_votes_percentage = round(((Raymon_count / total_votes) * 100), 3)
+
+        #candidates total votes
+        Diana_vote = Diana_count
+        Charles_vote = Charles_count
+        Raymon_vote = Raymon_count
+
 
     
     
@@ -43,7 +60,3 @@ with open(csvpath) as csvfile:
 
        
 
-
-
-print(f'Total Votes:  {str(total_votes)}')
-  
